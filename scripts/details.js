18bf510dff660c741bf407cceef3c5ddc3573bf5
Mike <mke.ort@gmail.com>
2020-05-24 19:44:28 -0500

    agregada la info a la vista

--- scripts/details.js
+++ scripts/details.js
@@ -3,9 +3,25 @@ function getPerson(id) {
     .then(res => res.json())
 }
 
+function getHomeWorld(link) {
+  return fetch(link)
+    .then(res => res.json())
+}
+
 async function displayPerson(id) {
   const person = await getPerson(id);
-  console.log(person);
+  const homeWorld = await getHomeWorld(person.homeworld);
+  document.querySelector('#name').value = person.name;
+  document.querySelector('#mass').value = person.mass;
+  document.querySelector('#hair_color').value = person.hair_color;
+  document.querySelector('#skin_color').value = person.skin_color;
+  document.querySelector('#eye_color').value = person.eye_color;
+  document.querySelector('#birth_year').value = person.birth_year;
+  document.querySelector('#gender').value = person.gender;
+  document.querySelector('#created').value = new Date(person.created).toLocaleDateString();
+  document.querySelector('#edited').value = new Date(person.edited).toLocaleDateString();
+  document.querySelector('#url').value = person.url;
+  document.querySelector('#homeworld').value = homeWorld.name;
 }
 
 function main() {
